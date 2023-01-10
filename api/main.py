1df4761c62f442d11c0a1832914d99e7e5e96b3a
shashank1623 <shashankbhardwaj2030@gmail.com>
2023-01-10 22:38:17 +0530

    Updated

--- api/main.py
+++ api/main.py
@@ -1,15 +1,42 @@
-from fastapi import FastAPI,File ,UploadFile
+from fastapi import FastAPI, File ,UploadFile
 import uvicorn
 import numpy as np
 from io import BytesIO
 from PIL import Image
 import tensorflow as tf
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
 
-MODEL = tf.keras.load_model("../saved_models/1")
-CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+MODEL  = tf.keras.models.load_model("../saved_models/3")
+CLASS_NAMES = ['Pepper__bell___Bacterial_spot',
+ 'Pepper__bell___healthy',
+ 'Potato___Early_blight',
+ 'Potato___Late_blight',
+ 'Potato___healthy',
+ 'Tomato_Bacterial_spot',
+ 'Tomato_Early_blight',
+ 'Tomato_Late_blight',
+ 'Tomato_Leaf_Mold',
+ 'Tomato_Septoria_leaf_spot',
+ 'Tomato_Spider_mites_Two_spotted_spider_mite',
+ 'Tomato__Target_Spot',
+ 'Tomato__YellowLeaf__Curl_Virus',
+ 'Tomato__mosaic_virus',
+ 'Tomato_healthy']
 @app.get("/ping")
 async def ping():
     return "Hello I'm alive"
@@ -26,13 +53,13 @@ async def predict(
     image = read_file_as_image(await file.read())
     img_batch = np.expand_dims(image,0)
     prediction = MODEL.predict(img_batch)
-
+    print(np.argmax(prediction[0]))
     predicted_class = CLASS_NAMES[np.argmax(prediction[0])]
-    confidence = np.max(prediciton[0])
+    confidence = np.max(prediction[0])
     return{
         'class':predicted_class,
         'confidence':float(confidence)
     }
 
 if __name__ =="__main__":
-    uvicorn.run(app,host='localhost',port=8080)
+    uvicorn.run(app,host='localhost',port=8000)
---
+++ t
@@ -0,0 +1,2 @@
+url: https://us-central1-fluid-axe-373804.cloudfunctions.net/predict
+npm run start
